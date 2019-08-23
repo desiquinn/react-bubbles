53baa473e6e1b6ecba6bd26a86da6672ae01b6b4
Desiree Morris <desiquinncompany@gmail.com>
2019-08-23 12:32:52 -0400

    formatted login form with out axios request

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,52 @@
-import React from "react";
+import React, {useState} from "react";
 
 const Login = () => {
+
+  const[credentials, setCredentials] = useState({
+                                          username: '',
+                                          password: '',
+                                        })
+
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+
+  const handleChange = (event) => {
+    // console.log (event.target.name, event.target.value)
+    // console.log (credentials)
+    setCredentials({
+      ...credentials, 
+      [event.target.name]: event.target.value
+    })
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(credentials)
+  }
+
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+      {/* <p>Login</p> */}
+      <form onSubmit={handleSubmit}>
+        <h2>Login</h2>
+        <label> Username: </label>
+          <input 
+            type='text'
+            name='username'
+            value={credentials.username}
+            onChange={handleChange}
+          />
+   
+        <label>Password:</label>
+          <input 
+            type='password'
+            name='password'
+            value={credentials.password}
+            onChange={handleChange}
+          />
+        <button type='submit'>Login</button>
+      </form>
     </>
   );
 };
